cb0b88e1133d46058789991212fb136c61eabf2e
ljss <450993438@qq.com>
2023-12-13 17:35:34 +0800

    Minor Fix

--- csrc/flash_attn/flash_api.cpp
+++ csrc/flash_attn/flash_api.cpp
@@ -197,7 +197,7 @@ void set_params_dgrad(Flash_bwd_params &params,
 void run_mha_fwd(Flash_fwd_params &params, cudaStream_t stream, bool force_split_kernel=false) {
     FP16_SWITCH(!params.is_bf16, [&] {
         FWD_HEADDIM_SWITCH(params.d, [&] {
-            if (params.num_splits <= 1 && !force_split_kernel) {  // If we don't set it num_splits == 0
+            if (kHeadDim <= 256 && params.num_splits <= 1 && !force_split_kernel) {  // If we don't set it num_splits == 0
                 run_mha_fwd_<elem_type, kHeadDim>(params, stream);
             } else {
                 run_mha_fwd_splitkv_dispatch<elem_type, kHeadDim>(params, stream);
