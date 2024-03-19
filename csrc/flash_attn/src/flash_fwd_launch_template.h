437be08ec3de2fee1b08a1c0a56e70eef1af8995
ljss <450993438@qq.com>
2024-03-19 11:03:06 +0800

    Optimize blocked_kvcache

--- csrc/flash_attn/src/flash_fwd_launch_template.h
+++ csrc/flash_attn/src/flash_fwd_launch_template.h
@@ -167,7 +167,7 @@ void run_mha_fwd_splitkv_dispatch(Flash_fwd_params &params, cudaStream_t stream)
         if (params.block_table != nullptr) assert(params.page_block_size == kBlockN);
         run_flash_splitkv_fwd<Flash_fwd_kernel_traits<Headdim, kBlockM, kBlockN, 4, false, false, T>>(params, stream);
     } else if (dprops->major == 9 && dprops->minor == 0) {  // H100
-        constexpr static int kBlockN = Headdim <= 64 ? 256 : (Headdim <= 128 ? 128 : 64);
+        constexpr static int kBlockN = Headdim <= 64 ? 256 : (Headdim <= 128 ? 128 : (Headdim <= 256 ? 64 : 32));
         if (params.block_table != nullptr) assert(params.page_block_size == kBlockN);
         run_flash_splitkv_fwd<Flash_fwd_kernel_traits<Headdim, kBlockM, kBlockN, 4, false, false, T>>(params, stream);
     } else {
